aa9b73950b3804c55ecb2aa4106a5634c8a26c65
ooobin <132541834+ooobin@users.noreply.github.com>
2023-11-30 12:01:33 +0800

    优化

--- src/demo/bpmn/bpmn.js
+++ src/demo/bpmn/bpmn.js
@@ -1,37 +1,43 @@
 import React, { Component } from "react";
 import BpmnModeler from "bpmn-js/lib/Modeler";
-import diagram from "./diagram.bpmn";
+import Diagram from "./diagram.bpmn";
 import "./App.css";
-
-// 左边工具栏及编辑元素
-import "bpmn-js/dist/assets/diagram-js.css";
+import "bpmn-js/dist/assets/diagram-js.css"; // 左边工具栏及编辑元素
 import "bpmn-js/dist/assets/bpmn-font/css/bpmn.css";
 import "bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css";
 import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
-
-// 右边属性面板
-import "bpmn-js-properties-panel/dist/assets/properties-panel.css";
+import "bpmn-js-properties-panel/dist/assets/properties-panel.css"; // 右边属性面板
 import {
     BpmnPropertiesPanelModule,
     BpmnPropertiesProviderModule,
     CamundaPlatformPropertiesProviderModule,
-} from "bpmn-js-properties-panel";
-
-// use Camunda BPMN namespace
+} from "bpmn-js-properties-panel"; // use Camunda BPMN namespace
 import camundaModdleDescriptors from "camunda-bpmn-moddle/resources/camunda";
+import axios from "axios";
 
 class Bpmn extends Component {
     constructor(props) {
         super(props);
+        this.state = {};
         this.bpmnModeler = null;
     }
 
     componentDidMount() {
-        // 初始化 bpmnModeler
+        // 初始化 BpmnModeler
         this.initModeler();
     }
 
-    initModeler = () => {
+    getDiagramXML = () => {
+        axios
+            .get("https://hexo-blog-1256114407.cos.ap-shenzhen-fsi.myqcloud.com/bpmnMock.bpmn")
+            .then((res) => {
+                this.setState({
+                    diagramXML: res.data,
+                });
+            });
+    };
+
+    initModeler = async () => {
         this.bpmnModeler = new BpmnModeler({
             container: "#canvas",
             height: "100vh",
@@ -48,16 +54,42 @@ class Bpmn extends Component {
             },
         });
 
+        // 获取 bpmn diagram
+        this.getDiagramXML();
         // 加载 bpmn diagram
         this.loadBpmnDiagram();
     };
 
-    loadBpmnDiagram = async () => {
-        try {
-            this.bpmnModeler.importXML(diagram);
-        } catch (error) {
-            console.error(error);
+    loadBpmnDiagram = () => {
+        if (this.state.diagramXML) {
+            this.bpmnModeler.importXML(Diagram);
+        } else {
+            this.bpmnModeler.importXML(this.state.diagramXML);
         }
+
+        this.importSuccess();
+    };
+
+    importSuccess = () => {
+        // 添加绑定事件
+        this.addModelerListener();
+    };
+
+    addModelerListener = () => {
+        this.bpmnModeler.on("commandStack.changed", (e) => {
+            console.log(e);
+        });
+    };
+
+    handleSaveXML = () => {
+        console.log("保存");
+        this.bpmnModeler.saveXML({ format: true }, (err, xml) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(xml);
+            }
+        });
     };
 
     render() {
@@ -67,6 +99,10 @@ class Bpmn extends Component {
                 <div id="canvas"></div>
                 {/* Additional panel for properties */}
                 <div id="properties"></div>
+
+                <button className="saveXML" onClick={this.handleSaveXML}>
+                    保存
+                </button>
             </div>
         );
     }
--- src/utils/http.js
+++ src/utils/http.js
@@ -1,39 +1,39 @@
-import axios from 'axios';
-import qs from 'qs';
+import axios from "axios";
+import qs from "qs";
 
-const url = document.getElementById('url').innerText;
+const url = document.getElementById("url").innerText;
 const baseURL = `http://${url}/`;
 
 // 创建自定义实例
 const http = axios.create({
-  baseURL, // 基础请求地址
-  timeout: 10000, // 请求超时设置
-  withCredentials: false, // 跨域请求是否需要携带 cookie
+    baseURL, // 基础请求地址
+    timeout: 10000, // 请求超时设置
+    withCredentials: false, // 跨域请求是否需要携带 cookie
 });
 
 // 请求拦截器
 http.interceptors.request.use(
-  (config) => {
-    console.log(config);
-    config.data = qs.stringify(config.data);
-    // 请求数据处理
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error.data.error.message);
-  },
+    (config) => {
+        console.log(config);
+        config.data = qs.stringify(config.data);
+        // 请求数据处理
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error.data.error.message);
+    },
 );
 
 // 响应拦截器
 http.interceptors.response.use(
-  (res) => {
-    console.log(res);
-    // 响应数据处理
-    return res.data;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
+    (res) => {
+        console.log(res);
+        // 响应数据处理
+        return res.data;
+    },
+    (error) => {
+        return Promise.reject(error);
+    },
 );
 
 export default http;
