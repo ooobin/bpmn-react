4a45180d1b30186b25c19aa986a139222f9eed4a
ooobin <132541834+ooobin@users.noreply.github.com>
2023-12-13 16:22:44 +0800

    函数组件学习

--- src/index.js
+++ src/index.js
@@ -1,49 +1,45 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Hello from './demo/demo00-hello';
-import SelectDemo from './demo/demo02-select';
-import TableDemo from './demo/demo03-table';
-import ModalDemo from './demo/demo04-modal';
-import CSS from './demo/demo06-css';
-import Echarts from './demo/demo08-echarts';
-import Parent from './demo/props/parent';
-import Home from './demo/demo09-home';
-import Test from './demo/demo-a-test';
-import Bpmn from './demo/bpmn/bpmn';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// 类组件
+import SelectDemo from "./demo/demo02-select";
+import TableDemo from "./demo/demo03-table";
+import ModalDemo from "./demo/demo04-modal";
+import CSS from "./demo/demo06-css";
+import Echarts from "./demo/demo08-echarts";
+import Parent from "./demo/props/parent";
+import Home from "./demo/demo09-home";
+import Test from "./demo/demo-a-test";
+import Bpmn from "./demo/bpmn/bpmn";
+
+// 函数组件
+import MyApp from "./demo/function/demo01-app";
+import MyGame from "./demo/function/demo02-game";
 
 class App extends React.Component {
-  render() {
-    // 转布尔
-    // const value = 1;
-    // console.log(!!value);
+    render() {
+        return (
+            <>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" exact element={<Home />} />
+                        <Route path="/select" exact element={<SelectDemo />} />
+                        <Route path="/table" exact element={<TableDemo />} />
+                        <Route path="/modal" exact element={<ModalDemo />} />
+                        <Route path="/css" exact element={<CSS />} />
+                        <Route path="/echarts" exact element={<Echarts />} />
+                        <Route path="/props" exact element={<Parent />} />
+                        <Route path="/test" exact element={<Test />} />
+                        <Route path="/bpmn" exact element={<Bpmn />} />
 
-    // 在 if 等条件语句中，将表达式的结果强制转换成布尔值是多余的
-    // if (value) {
-    //     console.log('value is true')
-    // }
-
-    return (
-      <>
-        <BrowserRouter>
-          <>
-            <Routes>
-              <Route path="/" exact element={<Home />} />
-              <Route path="/hello" exact element={<Hello />} />
-              <Route path="/select" exact element={<SelectDemo />} />
-              <Route path="/table" exact element={<TableDemo />} />
-              <Route path="/modal" exact element={<ModalDemo />} />
-              <Route path="/css" exact element={<CSS />} />
-              <Route path="/echarts" exact element={<Echarts />} />
-              <Route path="/props" exact element={<Parent />} />
-              <Route path="/test" exact element={<Test />} />
-              <Route path="/bpmn" exact element={<Bpmn />} />
-            </Routes>
-          </>
-        </BrowserRouter>
-      </>
-    );
-  }
+                        <Route path="/my-app" exact element={<MyApp />} />
+                        <Route path="/my-game" exact element={<MyGame />} />
+                    </Routes>
+                </BrowserRouter>
+            </>
+        );
+    }
 }
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById("root")).render(<App />);
