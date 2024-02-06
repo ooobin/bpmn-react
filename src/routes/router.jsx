18cf81ebefdb69593c8c9cc77d689ee6c56867d3
ooobin <132541834+ooobin@users.noreply.github.com>
2024-02-06 11:31:58 +0800

    feat: SignInSide页面

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import PageStructure from "../pages/page-structure";
 import Bpmn from "../pages/home/bpmn/bpmn";
 import Result_404 from "../pages/result/result-404";
-import SignIn from "../pages/home/sign-in.tsx"
+import SignIn from "../pages/home/sign-in/sign-in.tsx"
+import SignInSide from "../pages/home/sign-in/sign-in-side.tsx";
 
 const Router = () => {
     return (
@@ -13,7 +14,8 @@ const Router = () => {
                     <Route path="/" exact render={() => <Redirect to="/home" />} />
                     <Route path="/home" component={PageStructure} />
                     <Route path="/bpmn" exact component={Bpmn} />
-                    <Route path="/tsx" exact component={SignIn} />
+                    <Route path="/sign-in" exact component={SignIn} />
+                    <Route path="/sign-in-side" exact component={SignInSide} />
                     <Route path="*" component={Result_404} />
                 </Switch>
             </BrowserRouter>
