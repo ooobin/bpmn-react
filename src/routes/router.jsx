bffdb27ef9ffd82934b6423d1bd529c73797b915
ooobin <132541834+ooobin@users.noreply.github.com>
2024-02-06 10:59:12 +0800

    feat: MUI登录页面

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import PageStructure from "../pages/page-structure";
 import Bpmn from "../pages/home/bpmn/bpmn";
 import Result_404 from "../pages/result/result-404";
+import SignIn from "../pages/home/sign-in.tsx"
 
 const Router = () => {
     return (
@@ -12,6 +13,7 @@ const Router = () => {
                     <Route path="/" exact render={() => <Redirect to="/home" />} />
                     <Route path="/home" component={PageStructure} />
                     <Route path="/bpmn" exact component={Bpmn} />
+                    <Route path="/tsx" exact component={SignIn} />
                     <Route path="*" component={Result_404} />
                 </Switch>
             </BrowserRouter>
