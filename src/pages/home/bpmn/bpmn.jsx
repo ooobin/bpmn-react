daa13f49f4548a46b5548bda31c713c95c925ea4
ooobin <132541834+ooobin@users.noreply.github.com>
2024-03-06 22:16:18 +0800

    bpmn属性面板

--- src/pages/home/bpmn/bpmn.jsx
+++ src/pages/home/bpmn/bpmn.jsx
@@ -7,216 +7,220 @@ import Diagram from "./diagram.bpmn";
 import BpmnModeler from "bpmn-js/lib/Modeler";
 import customTranslate from "./translation/custom-translate";
 
-// 左边工具栏
+// 左侧工具栏相关
+import 'bpmn-js/dist/assets/bpmn-js.css';
 import "bpmn-js/dist/assets/diagram-js.css";
 import "bpmn-js/dist/assets/bpmn-font/css/bpmn.css";
 import "bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css";
 import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
 
-// 右边属性面板
-import 'bpmn-js-properties-panel/dist/assets/bpmn-js-properties-panel.css'
-import propertiesPanelModule from 'bpmn-js-properties-panel'
-import propertiesProviderModule from './lib/provider/camunda'
-import camundaModdleDescriptor from 'camunda-bpmn-moddle/resources/camunda'
+// 右侧属性面板
+import {
+  BpmnPropertiesPanelModule,
+  BpmnPropertiesProviderModule,
+  ZeebePropertiesProviderModule
+} from 'bpmn-js-properties-panel';
+import '@bpmn-io/properties-panel/assets/properties-panel.css';
+import ZeebeBpmnModdle from 'zeebe-bpmn-moddle/resources/zeebe.json'
 
 import { Button } from "antd";
 
 class Bpmn extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-        this.bpmnModeler = null;
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.bpmnModeler = null;
+  }
+
+  componentDidMount() {
+    this.initModeler().then(() => {
+      this.addBackgroundMovementListener();
+    });
+  }
+
+  /**
+   * 初始化 bpmn modeler
+   */
+  initModeler = async () => {
+    // Our custom translation module
+    // We need to use the array syntax that is used by bpmn-js internally
+    // 'value' tells bmpn-js to use the function instead of trying to instanciate it
+    const customTranslateModule = {
+      translate: ['value', customTranslate]
+    };
+
+    this.bpmnModeler = new BpmnModeler({
+      container: "#canvas",
+      height: "100vh",
+      propertiesPanel: {
+        parent: "#properties",
+      },
+      additionalModules: [
+        customTranslateModule,
+        BpmnPropertiesPanelModule,
+        BpmnPropertiesProviderModule,
+        ZeebePropertiesProviderModule
+      ],
+      moddleExtensions: {
+        zeebe: ZeebeBpmnModdle
+      }
+    });
+
+    await this.getDiagramXML();
+    this.loadBpmnDiagram();
+  }
+
+  /**
+   * 获取 bpmn diagram
+   * @returns {Promise<unknown>} 异步对象，具有三种状态（pending、fulfilled、rejected）
+   */
+  getDiagramXML = async () => {
+    const res = await axios.get("https://hexo-blog-1256114407.cos.ap-shenzhen-fsi.myqcloud.com/bpmnMock.bpmn");
+
+    return new Promise((resolve) => {
+      this.setState({
+        diagramXML: res.data,
+      }, () => {
+        resolve("success");
+      });
+    });
+  };
+
+  /**
+   * 加载 bpmn diagram
+   */
+  loadBpmnDiagram = () => {
+    if (this.state.diagramXML === undefined) {
+      // 返回一个默认的 diagram
+      this.bpmnModeler.importXML(Diagram);
+    } else {
+      this.bpmnModeler.importXML(this.state.diagramXML);
     }
 
-    componentDidMount() {
-        this.initModeler().then(() => {
-            this.addBackgroundMovementListener();
-        });
+    this.importSuccess();
+  };
+
+  /**
+   * 导入 bpmn diagram 成功后的箭头函数
+   */
+  importSuccess = () => {
+    this.addModelerListener();
+    this.addEventBusListener();
+  };
+
+  /**
+   * 添加 modeler 监听事件
+   */
+  addModelerListener = () => {
+    const events = ["shape.added", "shape.removed"];
+    events.forEach((event) => {
+      this.bpmnModeler.on(event, function (e) {
+      });
+    });
+  };
+
+  /**
+   * 监听 eventBus 事件
+   */
+  addEventBusListener = () => {
+    const eventTypes = ["element.click", "element.changed"];
+    const eventBus = this.bpmnModeler.get("eventBus", true);
+    eventTypes.forEach((eventType) => {
+      eventBus.on(eventType, function () {
+        },
+      );
+    });
+  };
+
+  /**
+   * 保存 bpmn diagram
+   */
+  handleSaveXML = () => {
+    this.bpmnModeler
+      .saveXML({ format: true })
+      .then((xml) => {
+        console.log(xml);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  /**
+   * 添加背景移动监听事件
+   */
+  addBackgroundMovementListener = () => {
+    const canvas = document.querySelector("#canvas");
+
+    // 以下代码块为触控板或滚轮移动时的背景图像移动
+    {
+      // 初始背景图片的位置
+      let x = 50,
+        y = 50;
+      let speedFactor = 76; // 调整这个值来改变背景移动的速度
+
+      canvas.addEventListener("wheel", (e) => {
+        x -= (e.deltaX / canvas.offsetWidth) * speedFactor;
+        y -= (e.deltaY / canvas.offsetHeight) * speedFactor;
+        canvas.style.backgroundPosition = `${x}% ${y}%`;
+      });
     }
 
-    /**
-     * 初始化 bpmn modeler
-     */
-    initModeler = async () => {
-        // Our custom translation module
-        // We need to use the array syntax that is used by bpmn-js internally
-        // 'value' tells bmpn-js to use the function instead of trying to instanciate it
-        const customTranslateModule = {
-            translate: ['value', customTranslate]
-        };
+    // 以下代码块为鼠标拖动时的背景图像移动
+    {
+      let isMouseDown = false;
+      canvas.addEventListener("mousedown", (e) => {
+        // 按下鼠标左键和滚轮时
+        if (e.button === 0 || e.button === 1) {
+          isMouseDown = true;
+        }
+      });
 
-        this.bpmnModeler = new BpmnModeler({
-            container: "#canvas",
-            height: "100vh",
-            propertiesPanel: {
-                parent: "#properties",
-            },
-            additionalModules: [
-                // 属性面板
-                propertiesPanelModule,
-                propertiesProviderModule,
-                customTranslateModule,
-            ],
-            moddleExtensions: {
-                //如果要在属性面板中维护camunda扩展：XXX属性，则需要此
-                camunda: camundaModdleDescriptor
-            }
-        });
+      canvas.addEventListener("mouseup", () => {
+        isMouseDown = false;
+      });
 
-        await this.getDiagramXML();
-        this.loadBpmnDiagram();
+      canvas.addEventListener("mousemove", (e) => {
+        if (isMouseDown) {
+          const x = (e.clientX / canvas.offsetWidth) * 100;
+          const y = (e.clientY / canvas.offsetHeight) * 100;
+          canvas.style.backgroundPosition = `${x}% ${y}%`;
+        }
+      });
     }
 
-    /**
-     * 获取 bpmn diagram
-     * @returns {Promise<unknown>} 异步对象，具有三种状态（pending、fulfilled、rejected）
-     */
-    getDiagramXML = async () => {
-        const res = await axios.get("https://hexo-blog-1256114407.cos.ap-shenzhen-fsi.myqcloud.com/bpmnMock.bpmn");
+    // 以下代码块为 Palette 抓手工具移动时的背景图像移动
+    {
+      let lastMousePosition = { x: 0, y: 0 };
 
-        return new Promise((resolve) => {
-            this.setState({
-                diagramXML: res.data,
-            }, () => {
-                resolve("success");
-            });
-        });
-    };
+      // 监听鼠标移动事件，更新鼠标位置
+      canvas.addEventListener("mousemove", function (e) {
+        lastMousePosition.x = e.clientX;
+        lastMousePosition.y = e.clientY;
+      });
 
-    /**
-     * 加载 bpmn diagram
-     */
-    loadBpmnDiagram = () => {
-        if (this.state.diagramXML === undefined) {
-            // 返回一个默认的 diagram
-            this.bpmnModeler.importXML(Diagram);
-        } else {
-            this.bpmnModeler.importXML(this.state.diagramXML);
-        }
-
-        this.importSuccess();
-    };
-
-    /**
-     * 导入 bpmn diagram 成功后的箭头函数
-     */
-    importSuccess = () => {
-        this.addModelerListener();
-        this.addEventBusListener();
-    };
-
-    /**
-     * 添加 modeler 监听事件
-     */
-    addModelerListener = () => {
-        const events = ["shape.added", "shape.removed"];
-        events.forEach((event) => {
-            this.bpmnModeler.on(event, function (e) {
-            });
-        });
-    };
-
-    /**
-     * 监听 eventBus 事件
-     */
-    addEventBusListener = () => {
-        const eventTypes = ["element.click", "element.changed"];
-        const eventBus = this.bpmnModeler.get("eventBus", true);
-        eventTypes.forEach((eventType) => {
-            eventBus.on(eventType, function () {},
-            );
-        });
-    };
-
-    /**
-     * 保存 bpmn diagram
-     */
-    handleSaveXML = () => {
-        this.bpmnModeler
-            .saveXML({ format: true })
-            .then((xml) => {
-                console.log(xml);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
-
-    /**
-     * 添加背景移动监听事件
-     */
-    addBackgroundMovementListener = () => {
-        const canvas = document.querySelector("#canvas");
-
-        // 以下代码块为触控板或滚轮移动时的背景图像移动
-        {
-            // 初始背景图片的位置
-            let x = 50,
-                y = 50;
-            let speedFactor = 76; // 调整这个值来改变背景移动的速度
-
-            canvas.addEventListener("wheel", (e) => {
-                x -= (e.deltaX / canvas.offsetWidth) * speedFactor;
-                y -= (e.deltaY / canvas.offsetHeight) * speedFactor;
-                canvas.style.backgroundPosition = `${x}% ${y}%`;
-            });
-        }
-
-        // 以下代码块为鼠标拖动时的背景图像移动
-        {
-            let isMouseDown = false;
-            canvas.addEventListener("mousedown", (e) => {
-                // 按下鼠标左键和滚轮时
-                if (e.button === 0 || e.button === 1) {
-                    isMouseDown = true;
-                }
-            });
-
-            canvas.addEventListener("mouseup", () => {
-                isMouseDown = false;
-            });
-
-            canvas.addEventListener("mousemove", (e) => {
-                if (isMouseDown) {
-                    const x = (e.clientX / canvas.offsetWidth) * 100;
-                    const y = (e.clientY / canvas.offsetHeight) * 100;
-                    canvas.style.backgroundPosition = `${x}% ${y}%`;
-                }
-            });
-        }
-
-        // 以下代码块为 Palette 抓手工具移动时的背景图像移动
-        {
-            let lastMousePosition = { x: 0, y: 0 };
-
-            // 监听鼠标移动事件，更新鼠标位置
-            canvas.addEventListener("mousemove", function (e) {
-                lastMousePosition.x = e.clientX;
-                lastMousePosition.y = e.clientY;
-            });
-
-            this.bpmnModeler.on("hand.move.move", function () {
-                const x = (lastMousePosition.x / canvas.offsetWidth) * 100;
-                const y = (lastMousePosition.y / canvas.offsetHeight) * 100;
-                canvas.style.backgroundPosition = `${x}% ${y}%`;
-            });
-        }
-    };
-
-    render() {
-        return (
-            <div id="App">
-                <div id="canvas">
-                    <div class="canvas-toolbox">
-                        <Button onClick={this.handleSaveXML}>
-                            保存
-                        </Button>
-                    </div>
-                </div>
-                <div id="properties"></div>
-            </div>
-        );
+      this.bpmnModeler.on("hand.move.move", function () {
+        const x = (lastMousePosition.x / canvas.offsetWidth) * 100;
+        const y = (lastMousePosition.y / canvas.offsetHeight) * 100;
+        canvas.style.backgroundPosition = `${x}% ${y}%`;
+      });
     }
+  };
+
+  render() {
+    return (
+      <div id="App">
+        <div id="canvas">
+          <div class="canvas-toolbox">
+            <Button onClick={this.handleSaveXML}>
+              保存
+            </Button>
+          </div>
+        </div>
+        <div id="properties"></div>
+      </div>
+    );
+  }
 }
 
 export default Bpmn;
